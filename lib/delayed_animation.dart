ed077e1f261a871eccaabd82cbd9885405832bc4
Coolhacker009 <131943685+Coolhacker009@users.noreply.github.com>
2023-10-30 18:57:42 +0000

    splash1

--- lib/delayed_animation.dart
+++ lib/delayed_animation.dart
@@ -3,7 +3,7 @@ import "dart:async";
 
 class DelayedAnimation extends StatefulWidget {
   final Widget child;
-  final Widget delay;
+  final int delay;
 
   const DelayedAnimation({required this.delay, required this.child});
 
@@ -13,8 +13,39 @@ class DelayedAnimation extends StatefulWidget {
 
 class _DelayedAnimationState extends State<DelayedAnimation>
     with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _animOffset;
   @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+
+    final curve = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.decelerate,
+    );
+
+    _animOffset = Tween<Offset>(
+      begin: Offset(0.0, 0.5),
+      end: Offset.zero,
+    ).animate(curve);
+
+    Timer(Duration(milliseconds: widget.delay), () {
+      _controller.forward();
+    });
+  }
+
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return FadeTransition(
+      opacity: _controller,
+      child: SlideTransition(
+        position: _animOffset,
+        child: widget.child,
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:pro_cv/delayed_animation.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,11 +17,33 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-            child: Image(
-          image: AssetImage("assets/img/logo.png"),
-        )),
-      ),
+          body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            child: DelayedAnimation(
+              delay: 1000,
+              child: Image.asset(
+                "assets/img/logo.png",
+                height: 250,
+                width: 250,
+              ),
+            ),
+          ),
+          Container(
+              child: DelayedAnimation(
+            delay: 1500,
+            child: Text(
+              "Crééz un CV professionnel n'a jamais été facile",
+              textAlign: TextAlign.center,
+              style: GoogleFonts.poppins(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: const Color.fromARGB(255, 75, 73, 73)),
+            ),
+          ))
+        ],
+      )),
     );
   }
 }
